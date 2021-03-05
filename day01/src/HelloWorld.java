530f5ad8a1f9d525d1e0219cb7e44c661f275111
LuckMia <Luck-Mia@outlook.com>
2021-03-05 18:18:49 +0800

    第一个Java程序

---
+++ day01/src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: 晴天
+ * @Date: 2021/03/05/17:53
+ * @Description:
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // 打印输出 “Hello World！”
+        System.out.println("Hello World!");
+    }
+}
